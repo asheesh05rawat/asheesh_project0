d420dce7f283ac6fb69afce8211569cc9bf0d562
asheesh05rawat <asheesh511rawat@gmail.com>
2025-08-25 18:58:16 +0530

    list.py

---
+++ list.py
@@ -0,0 +1,27 @@
+## lists ------------------------
+arr=[98,78,45,25,100]
+print(arr)
+print(len(arr))
+print(type(arr))  
+
+
+csk=["dhoni","rania","bravo","jaddu","LB morkel"]
+print(csk)
+
+#-------------- example of lists in pyrhon are mutable ( means changeable )---------------
+csk[0]="deepak chahar"
+print(csk)
+
+#-----------example tha lists in python can store any lement of any data_type in it----------
+rcb=["chokli",49,49.0]
+print(rcb)
+
+
+## difference between list and tupple
+li=["asheesh",1,2,4.5,"a"]
+tu=("rawat",8,9,"fuck")
+li[0]="sawar"
+print(li)
+print(tu)
+print(type(tu))
+print(type(li))
